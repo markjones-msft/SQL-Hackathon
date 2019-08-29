ac42613fa72cb1be266c0634cf1e83914029e649
markjones-msft <54671949+markjones-msft@users.noreply.github.com>
2019-08-29 15:11:24 +0100

    Create ARM Deployment - SQLHAckathon v1.0.ps1
    Test Network

---
+++ ARM Templates/ARM Deployment - SQLHAckathon v1.0.ps1
@@ -0,0 +1,20 @@
+﻿#Login-AzAccount
+
+
+###################################################################
+# Setup Hack Resource Groups
+###################################################################
+
+$SharedRG = "SQLHACK-SHARED"
+$TeamRG = "SQLHACK-TEAM_VMs"
+$Location = "NorthEurope"
+
+New-AzResourceGroup -Name $SharedRG -Location $Location
+New-AzResourceGroup -Name $TeamRG -Location $Location
+
+###################################################################
+# Setup Network
+###################################################################
+New-AzResourceGroupDeployment `
+-ResourceGroupName $SharedRG `
+-TemplateUri https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Network%20-%20v1.0.json
