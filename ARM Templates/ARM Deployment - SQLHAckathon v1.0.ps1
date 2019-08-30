297de3650cd6271c84359692deb39aee9730c1d7
markjones-msft <54671949+markjones-msft@users.noreply.github.com>
2019-08-30 20:01:50 +0100

    Beta Release
    Beta Release

--- ARM Templates/ARM Deployment - SQLHAckathon v1.0.ps1
+++ ARM Templates/ARM Deployment - SQLHAckathon v1.0.ps1
@@ -1,5 +1,13 @@
-﻿#Login-AzAccount
+﻿[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$True)]
+   [string]$SubscriptionId,
+   [int]$TeamVMCount
+)
 
+#Login-AzAccount
+
+Select-AzSubscription -SubscriptionId $SubscriptionId
 
 ###################################################################
 # Setup Hack Resource Groups
@@ -7,14 +15,63 @@
 
 $SharedRG = "SQLHACK-SHARED"
 $TeamRG = "SQLHACK-TEAM_VMs"
+#$ResourcesRG = "SQLHACK-RESOURCES"
+
 $Location = "NorthEurope"
+
+# Check Resource Groups do not already exist
+Get-AzResourceGroup -name $SharedRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if (!($notPresent)) {Write-Warning "Resource Group $SharedRG already exisits. Please check retry"; return;}
+
+Get-AzResourceGroup -name $TeamRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if (!($notPresent)) {Write-Warning "Resource Group $TeamRG already exisits. Please check retry"; return;}
+Get-AzResourceGroup -
 
 New-AzResourceGroup -Name $SharedRG -Location $Location
 New-AzResourceGroup -Name $TeamRG -Location $Location
+
 
 ###################################################################
 # Setup Network
 ###################################################################
 New-AzResourceGroupDeployment `
 -ResourceGroupName $SharedRG `
--TemplateUri https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Network%20-%20v1.0.json
+-TemplateFile "C:\Users\markjon\Documents\GitHub\SQL-Hackathon\ARM Templates\ARM Template - SQL Hackathon - Network - RC.json" `
+-Name "NetworkBuild"
+
+# Check if Vnet has been created
+Get-AzVirtualNetwork -Name SQLHACK-SHARED-vnet -ResourceGroupName $SharedRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent) {Write-Warning "VNET Failed to build. Please check and retry";return;}
+
+###################################################################
+# 1.Setup Shared Resources
+###################################################################
+New-AzResourceGroupDeployment `
+-ResourceGroupName "SQLHACK-SHARED" `
+-TemplateFile "C:\Users\markjon\Documents\GitHub\SQL-Hackathon\ARM Templates\ARM Template - SQL Hackathon - Shared - v2.0.json" `
+-Name "SQLLegacyBuild"
+
+#TODO Check Resources Build ok here
+
+Write-Warning "DataFactory Build Ok. You will need to start the integration runtime and enable AHUB"
+
+# Setup KeyVault 
+New-AzKeyVault -Name sqlhack-keyvault -ResourceGroupName $SharedRG -Location $Location -EnableSoftDelete
+
+Get-AzKeyVault -Name sqlhack-keyvault -ResourceGroupName $SharedRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent) {Write-Warning "sqlhack-keyvault Failed to build. Please check and retry";return;}
+
+
+###################################################################
+# 2. Setup Team Workstations
+###################################################################
+New-AzResourceGroupDeployment `
+-ResourceGroupName $TeamRG `
+-TemplateFile "C:\Users\markjon\Documents\GitHub\SQL-Hackathon\ARM Templates\ARM Template - SQL Hackathon Jump Servers - v2.1.json" `
+-Name "TeamVMBuild" `
+-vmCount $TeamVMCount
+
+
+
+
+
---
+++ ARM Templates/ARM Deployment - TEAM VM Build v1.0.ps1
@@ -0,0 +1,43 @@
+﻿function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
+# Disable IE ESC
+Disable-InternetExplorerESC
+
+#Create Folders for Labs and Installs
+md -Path 'C:\SQLHACK'
+md -Path 'C:\Install'
+md -Path 'C:\SQLHACK\LABS'
+md -Path 'C:\SQLHACK\LABS\01-Data Migration'
+md -Path 'C:\SQLHACK\LABS\02-SSIS Migration'
+md -Path 'C:\SQLHACK\LABS\03-Security'
+
+# Download and install SQL Server Management Studio
+Invoke-WebRequest 'https://go.microsoft.com/fwlink/?linkid=2088649' -OutFile 'C:\Install\SSMS-Setup.exe'
+$pathArgs = {C:\Install\SSMS-Setup.exe /S /v/qn}
+Invoke-Command -ScriptBlock $pathArgs
+
+
+# Download and install Data Mirgation Assistant
+Invoke-WebRequest 'https://download.microsoft.com/download/C/6/3/C63D8695-CEF2-43C3-AF0A-4989507E429B/DataMigrationAssistant.msi' -OutFile 'C:\Install\DataMigrationAssistant.msi'
+Start-Process -file 'C:\Install\DataMigrationAssistant.msi' -arg '/qn /l*v C:\Install\dma_install.txt' -passthru | wait-process
+
+
+# Download and install SSDT
+Invoke-WebRequest 'https://go.microsoft.com/fwlink/?linkid=2095463' -OutFile 'C:\SSDT-Setup-ENU.exe'
+$pathArgs = {C:\SSDT-Setup-ENU.exe /S /v/qn}
+Invoke-Command -ScriptBlock $pathArgs
+
+
+
+
+
+
+
+
