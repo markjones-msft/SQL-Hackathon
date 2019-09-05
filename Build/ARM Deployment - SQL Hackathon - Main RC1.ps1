9688ba71abad8b38e30b6fb51a76c1b47b3b26ef
markjones-msft <54671949+markjones-msft@users.noreply.github.com>
2019-09-05 07:36:42 +0100

    Update ARM Deployment - SQL Hackathon - Main RC1.ps1

--- Build/ARM Deployment - SQL Hackathon - Main RC1.ps1
+++ Build/ARM Deployment - SQL Hackathon - Main RC1.ps1
@@ -15,19 +15,16 @@ Select-AzSubscription -SubscriptionId $SubscriptionId
 
 $SharedRG = "SQLHACK-SHARED"
 $TeamRG = "SQLHACK-TEAM_VMs"
-#$ResourcesRG = "SQLHACK-RESOURCES"
-
 $Location = "NorthEurope"
 
 # Check Resource Groups do not already exist
-Get-AzResourceGroup -name $SharedRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
-if (!($notPresent)) {Write-Warning "Resource Group $SharedRG already exisits. Please check retry"; return;}
+#Get-AzResourceGroup -name $SharedRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
+#if (!($notPresent)) {Write-Warning "Resource Group $SharedRG already exisits. Please check retry"; return;}
 
-Get-AzResourceGroup -name $TeamRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
-if (!($notPresent)) {Write-Warning "Resource Group $TeamRG already exisits. Please check retry"; return;}
-Get-AzResourceGroup -
+#Get-AzResourceGroup -name $TeamRG -ErrorVariable notPresent -ErrorAction SilentlyContinue
+#if (!($notPresent)) {Write-Warning "Resource Group $TeamRG already exisits. Please check retry"; return;}
 
-New-AzResourceGroup -Name $SharedRG -Location $Location
+New-AzResourceGroup -Name $SharedRG -Location $Location 
 New-AzResourceGroup -Name $TeamRG -Location $Location
 
 
@@ -36,7 +33,7 @@ New-AzResourceGroup -Name $TeamRG -Location $Location
 ###################################################################
 New-AzResourceGroupDeployment `
 -ResourceGroupName $SharedRG `
--TemplateFile "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Network%20-%20RC1.json" `
+-TemplateUri "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Network%20-%20RC1.json" `
 -Name "NetworkBuild"
 
 # Check if Vnet has been created
@@ -48,8 +45,8 @@ if ($notPresent) {Write-Warning "VNET Failed to build. Please check and retry";r
 ###################################################################
 New-AzResourceGroupDeployment `
 -ResourceGroupName "SQLHACK-SHARED" `
--TemplateFile "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Shared%20-%20v2.0.json" `
--Name "SQLLegacyBuild"
+-TemplateUri "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Shared%20-%20v2.0.json" `
+-Name "SharedServicesBuild"
 
 #TODO Check Resources Build ok here
 
@@ -69,7 +66,7 @@ $TeamVMCount = 01
 ###################################################################
 New-AzResourceGroupDeployment `
 -ResourceGroupName $TeamRG `
--TemplateFile "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Jump%20Servers%20-%20v2.5.json" `
+-TemplateUri "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Jump%20Servers%20-%20v2.5.json" `
 -Name "TeamVMBuild" `
 -vmCount $TeamVMCount
 
