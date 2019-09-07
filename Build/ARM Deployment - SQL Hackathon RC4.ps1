d764d48f7b5bbd9de59bb085b87469c663d96887
markjones-msft <54671949+markjones-msft@users.noreply.github.com>
2019-09-07 22:32:25 +0100

    RC

--- Build/ARM Deployment - SQL Hackathon RC4.ps1
+++ Build/ARM Deployment - SQL Hackathon RC4.ps1
@@ -67,8 +67,8 @@ $DefaultValue = 5
 if (($TeamVMCount = Read-Host "Please enter the number of Team VM's required (1-20) (default value: $DefaultValue)") -eq '') {$TeamVMCount = $DefaultValue}
 If ($TeamVMCount -gt 20)
 {
-    Write-Warning "Maximum number TEAM VM's is 20. Setting to 20 VM's"
-    $TeamVMCount = 20
+    Write-Warning "Maximum number TEAM VM's is 20. Setting to 5 VM's"
+    $TeamVMCount = 5
 
 }
 
@@ -164,10 +164,10 @@ if ($notPresent) {Write-Warning "sqlhack-keyvault Failed to build. Please check 
 ###################################################################
 # Setup Team VM's
 ###################################################################
-
 Write-Host -BackgroundColor Black -ForegroundColor Yellow "Creating $TeamVMCount Team Server(s).................................................."
+
 $TemplateUri = "https://raw.githubusercontent.com/markjones-msft/SQL-Hackathon/master/Build/ARM%20Templates/ARM%20Template%20-%20SQL%20Hackathon%20-%20Jump%20Servers%20-%20RC4.json"
-Run-ARMTemplate  -ResourceGroupName $TeamRG -TemplateUri $TemplateUri -Name "TeamVMBuild22" -vmCount $TeamVMCount -SharedResourceGroup $SharedRG -SASURIKey $JsonSASURI -StorageAccount $StorageAccount
+Run-ARMTemplate  -ResourceGroupName $TeamRG -TemplateUri $TemplateUri -Name "TeamVMBuild" -vmCount $TeamVMCount -SharedResourceGroup $SharedRG -SASURIKey $JsonSASURI -StorageAccount $StorageAccount
 
 
 ###################################################################
